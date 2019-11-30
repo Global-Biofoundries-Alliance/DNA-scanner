721242c5c52264269c7a652990bfdffda2dd4878
canislepus <eggileierkopf@gmail.com>
2019-11-30 15:41:19 +0100

    Implemented some basic functionality. NOT FULLY OPERATIONAL YET!

---
+++ Backend/Controller/dataformats.py
@@ -0,0 +1,26 @@
+class SearchResponse:
+        size = 10
+        count = 150
+        offset = 1
+        result = [
+            {
+                "offers": [
+                    {
+                        "vendorinformation": {
+                            "name": "TWIST DNA ...",
+                            "shortname": "TWIST",
+                            "key": ""
+                        },
+                        "price": "0.05",
+                        "turnovertime": "10"
+                    }
+                ],
+                "sequenceinformation": {
+                    "id": "1233-4566",
+                    "name": "Waschmittel",
+                    "sequence": "ACTG"
+                }
+            }
+        ]
+        message = ["TWIST API currently unavailable"]
+

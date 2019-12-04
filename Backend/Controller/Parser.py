b5c40550884c77788f950dcc3987092440282981
gledidoci <docigledi@gmail.com>
2019-12-04 21:21:21 +0100

    Fixed SBOL Parse

--- Backend/Controller/Parser.py
+++ Backend/Controller/Parser.py
@@ -17,7 +17,7 @@ def parse(inputFileName):
     if(fileType == "fasta" or fileType == "genbank"):
         return parseFile(inputFileName,fileType)
     if(fileType == "sbol"):
-        return getSBOL(inputFileName)
+        return parseSBOL(inputFileName)
         
 def getFileType(inputFileName):
     if(inputFileName.endswith('.gb') or inputFileName.endswith('.gbk')):
@@ -40,55 +40,17 @@ def parseFile(inputFile, fileFormat):
         returnList.append(sequence)
     return returnList
 
-def getSBOL(inputFile):
-    file = open(inputFile).read()
-    request = { 'options': {'language' : 'SBOL2',
-                            'test_equality': False,
-                            'check_uri_compliance': False,
-                            'check_completeness': False,
-                            'check_best_practices': False,
-                            'fail_on_first_error': False,
-                            'provide_detailed_stack_trace': False,
-                            'subset_uri': '',
-                            'uri_prefix': '',
-                            'version': '',
-                            'insert_type': False,
-                            'main_file_name': 'main file',
-                            'diff_file_name': 'comparison file',
-                                    },
-                'return_file': True,
-                'main_file': file
-              }
-    resp = requests.post("https://validator.sbolstandard.org/validate/", json = request)
-    result = resp.json()['result']
-    result_file = "res.xml"
-    text_file = open(result_file, "w")
-    n = text_file.write(result)
-    text_file.close()
-    return parseSBOL(result_file)
-    
+  
 def parseSBOL(inputString):
-    doc = sbol.Document()
-    doc.read(inputString)
-    #print(doc)
-    #doc.write('aa.xml')
-    #print(len(doc))
     i = 0
+    doc = sbol.Document()
+    doc.read(inputFile)
     returnList = []
     for a in doc.sequences:
         sequence = SeqObject()
-        sequence.idN = "NoId"
-        sequence.name = "NoName"
+        sequence.idN = a.displayId
+        sequence.name = "Sequence " + str(i)
         sequence.sequence = str(a.elements).upper()
         returnList.append(sequence)
-    #print(returnList[0].idN)
-    #print(returnList[0].name)
-    #print(returnList[0].sequence)
-    #print("~~~~~~~~~~~~~~~~~~~~~~")
-    #for cd in doc.componentDefinitions:
-    #    returnList[i].idN = cd.displayId
-    #    i += 1
-    #print(returnList[0].idN)
-    #print(returnList[0].name)
-    #print(returnList[0].sequence)
+        i = i + 1
     return returnList

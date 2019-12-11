30a1e5a77a4fba22ede81949ac6f252049d6f231
canislepus <eggileierkopf@gmail.com>
2019-12-11 13:17:10 +0100

    Sequence file upload may be operational as soon as libsbol works

--- Backend/Controller/routes.py
+++ Backend/Controller/routes.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, json, flash
+import tempfile
+
+from flask import request, json, flash
+from werkzeug.utils import secure_filename
+
 from .app import app
 from .dataformats import SearchResponse
-
+from .parser import parse
 
 @app.route('/upload', methods=['post'])
 def uploadFile():
@@ -13,5 +17,7 @@ def uploadFile():
     #
     #   TODO: Arr, Here be stuff for populating the response object such as file parsing!
     #
+    tempf = tempfile.mkdtemp('.' + secure_filename(request.files['seqfile'].filename).rsplit('.', 1)[1].lower())
+    parse(tempf)
 
     return json.jsonify(resp.__dict__)

1e20c59a82860ea15cd2a8197f3031071e3e427e
gledidoci <docigledi@gmail.com>
2019-12-12 23:12:43 +0100

    new structure

--- Backend/Pinger/GeneArtClient.py
+++ Backend/Pinger/GeneArtClient.py
@@ -1,49 +1,68 @@
-import json
-import requests
-import yaml
-
-cfg_geneart = {}
-
-def getConfig():
-    global cfg_geneart
-    with open("config.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
-    cfg_geneart = cfg['geneart']
-
-def statOrAddToCart(projectId, action):
-    request = {
-        "authentication":
-     { "username": cfg_geneart['username'],
-       "token": cfg_geneart['token']
-     }, 
-     "projectId": projectId
-    }
-    destination = getDestination(action)
-    resp = requests.post(destination, json = request)
-    result = resp.json()
-    if(resp.status_code == 400 or resp.status_code == 500):
-        printError(result, resp.status_code)
-    else:
-        print("ProjectId: " + str(result["projectId"]))
-        statusField = str(result["status"])
-        print("Status: " + statusField)
-        if(not(statusField == "draft")):
-            print("CartId: " + str(result["cartId"]))
-
-            
-def getDestination(action):
-    if(action == "validate"):
-        return cfg_geneart['server'] + cfg_geneart['validate']
-    if(action == "status"):
-        return cfg_geneart['server'] + cfg_geneart['status']
-    if(action == "addToCart"):
-        return cfg_geneart['server'] + cfg_geneart['addToCart']
-    if(action == "upload"):
-        return cfg_geneart['server'] + cfg_geneart['upload']
-
-def printError(resultJson, code):
-    if(code==400):
-        print("ErrorType: " + str(resultJson["errortype"]))
-        print("Details: " + str(resultJson["details"]))
-    if(code == 500):
-        print("Details: " + str(resultJson["details"]))
+class GeneArtClient:
+    server = ""
+    validate = ""
+    status = ""
+    addToCart = ""
+    upload = ""
+    username = ""
+    token = ""
+    diagnostics = ""
+    validAcc = False
+    conf = {}
+    
+    def __init__(self): 
+        with open("../config.yml", 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+        self.conf = cfg['pinger']['geneart']
+        self.server = self.conf['server']
+        self.validate = self.conf['validate']
+        self.status = self.conf['status']
+        self.addToCart = self.conf['addToCart']
+        self.upload = self.conf['upload']
+        self.username = self.conf['username']
+        self.token = self.conf['token']
+        self.diagnostics = "https://www.thermofisher.com/order/gene-design-services/api/diagnostics/v1/bulk?waitSec=60&forceRecompute=true"
+        self.validAcc = self.auth()
+        if(self.validAcc == False):
+            raise Exception('User Credentials are wrong')
+        
+    def destination(self, action):
+        if(action == "validate"):
+            return self.server + self.validate
+        if(action == "status"):
+            return self.server + self.status
+        if(action == "addToCart"):
+            return self.server + self.addToCart
+        if(action == "upload"):
+            return self.server + self.upload
+    
+    def getAuthPart(self, projectId):
+        request = {
+            "authentication":
+         { "username": self.username,
+           "token": self.token
+         }, 
+         "projectId": projectId
+        }
+        return request
+    
+    def auth(self):
+        result = self.statusRev("2019AAAAAX")
+        if("errortype" in result.keys() and result["errortype"] == "authenticationFailed"):
+            return False
+        else:
+            return True
+    
+    def statusRev(self, projectId):
+        request = self.getAuthPart(projectId)
+        dest = self.destination("status")
+        resp = requests.post(dest, json = request)
+        result = resp.json()
+        return result
+    
+    def addtocrt(self, projectId):
+        request = self.getAuthPart(projectId)
+        dest = self.destination("addToCart")
+        resp = requests.post(dest, json = request)
+        result = resp.json()
+        return result

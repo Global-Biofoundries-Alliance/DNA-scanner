59e987aaae11231e170e02c9a28ccd96a341724b
paul.schickling <paul.schickling@schiggomail.de>
2019-12-22 12:11:55 +0100

    Added comments

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+echo "Copy nginx configuration to /srv/dnascanner/nginx/ "
 mkdir /srv/dnascanner/nginx -p
 cp nginx-secure.conf /srv/dnascanner/nginx/
 
+echo "Create directory /srv/dnascanner/cert/ for certificates"
 mkdir /srv/dnascanner/cert
 
+echo "Start container"
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --force-recreate 

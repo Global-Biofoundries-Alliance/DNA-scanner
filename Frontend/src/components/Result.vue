3e2c752f25b80f3967400ff7bb7c9616639d2f34
Administrator <k-yash@hotmail.de>
2020-01-24 17:46:37 +0100

    Matrix is created as discussed

--- Frontend/src/components/Result.vue
+++ Frontend/src/components/Result.vue
@@ -1,35 +1,5 @@
 <template>
     <div>
-        <!--        <v-container style="margin-right: 0; margin-left: 600px; margin-top: 30px">-->
-        <!--            <v-row>-->
-        <!--                <v-card width="200px" style="text-align: center" tile outlined class="pa-6" v-for="vendor in vendors" :key="vendor.name">-->
-        <!--                    {{vendor.name}}-->
-        <!--                </v-card>-->
-        <!--            </v-row>-->
-        <!--            <v-row>-->
-        <!--                <v-card width="200px" tile outlined class="pa-2 mt-12" v-for="i in vendors.length" :key="i">-->
-        <!--                    {{i}}-->
-        <!--                </v-card>-->
-        <!--            </v-row>-->
-        <!--        </v-container>-->
-        <!--        <v-container style="margin-top: 50px">-->
-        <!--            <v-row class="mb-4">-->
-        <!--                    <v-card width="110px" tile outlined class="pa-2" style="border-right: 0">-->
-        <!--                        ID-->
-        <!--                    </v-card>-->
-        <!--                    <v-card width="150px" tile outlined class="pa-2" style="border-left: 0">-->
-        <!--                        Name-->
-        <!--                    </v-card>-->
-        <!--            </v-row>-->
-        <!--            <v-row v-for="(n,i) in 10" :key="i">-->
-        <!--                    <v-card width="110px" tile outlined class="pa-2" style="border-right: 0">-->
-        <!--                        {{result[i].sequenceinformation.id}}-->
-        <!--                    </v-card>-->
-        <!--                    <v-card width="150px" tile outlined class="pa-2" style="border-left: 0">-->
-        <!--                        {{result[i].sequenceinformation.name}}-->
-        <!--                    </v-card>-->
-        <!--            </v-row>-->
-        <!--        </v-container>-->
         <v-container>
             <v-row>
                 <v-col cols="4" style="margin-top: 100px">
@@ -41,7 +11,7 @@
                             Name
                         </v-card>
                     </v-row>
-                    <v-row v-for="(n,i) in 1" :key="i">
+                    <v-row v-for="(n,i) in resultBody.count" :key="i">
                         <v-card width="110px" tile outlined class="pa-2" style="border-right: 0">
                             {{result[i].sequenceinformation.id}}
                         </v-card>
@@ -66,29 +36,45 @@
                             </v-card-actions>
                         </v-card>
                     </v-row>
-                    <v-row v-for="(n,i) in 1" :key="i">
+                    <v-row v-for="(n,i) in resultBody.count" :key="i">
                         <v-hover v-slot:default="{ hover }" v-for="(k,j) in vendors.length" :key="j">
                             <v-card width="200px" class="pt-2 pb-2" tile outlined
-                                    :elevation="(hover || permanentElevation[i][j]) ? 16 : 0">
+                                    :elevation="(hover || result[i].vendors[j].offers[0].selected) ? 16 : 0">
                                 <v-card-actions style="padding-top: 2px; padding-bottom: 2px">
                                     <v-card-text class="ml-2 pa-0" style="font-size: 20px">
-                                        {{j}}
+                                        {{result[i].vendors[j].offers[0].price}}
                                     </v-card-text>
                                     <v-spacer></v-spacer>
                                     <v-card-text class="mr-1 pa-0" style="text-align: right; font-size: 20px">
-                                        {{j + 1}}
+                                        {{result[i].vendors[j].offers[0].turnoverTime}}
                                     </v-card-text>
                                 </v-card-actions>
 
-                                <v-expand-transition v-if="permanentElevation[i][j] === true">
+                                <v-expand-transition v-if="result[i].vendors[j].offers[0].selected === true">
                                     <div
                                             v-if="hover"
                                             class="d-flex transition-fast-in-fast-out grey lighten-2 v-card--reveal display-3"
                                             style="height: 100%; width: 100%"
                                     >
-                                        <v-icon class="ml-3" size="22px" @click="permanentElevation[i][j] = false">
+                                        <v-icon class="ml-3" size="22px"
+                                                @click="result[i].vendors[j].offers[0].selected = false">
                                             mdi-minus
                                         </v-icon>
+                                    </div>
+                                </v-expand-transition>
+
+                                <v-expand-transition v-else-if="result[i].vendors[j].offers.length > 1">
+                                    <div
+                                            v-if="hover"
+                                            class="d-flex transition-fast-in-fast-out grey lighten-2 v-card--reveal display-3"
+                                            style="height: 100%; width: 100%"
+                                    >
+
+                                        <v-icon class="ml-3" size="22px"
+                                                @click="result[i].vendors[j].offers[0].dialog = true">
+                                            mdi-dots-horizontal
+                                        </v-icon>
+
                                     </div>
                                 </v-expand-transition>
 
@@ -98,16 +84,53 @@
                                             class="d-flex transition-fast-in-fast-out grey lighten-2 v-card--reveal display-3"
                                             style="height: 100%; width: 100%"
                                     >
-                                        <v-icon class="ml-3" size="22px" @click="permanentElevation[i][j] = true">
+                                        <v-icon class="ml-3" size="22px"
+                                                @click="result[i].vendors[j].offers[0].selected = true">
                                             mdi-plus
                                         </v-icon>
                                     </div>
                                 </v-expand-transition>
+
+<!--                                <v-dialog v-model="dialog[i][j]" scrollable max-width="300px">-->
+<!--                                    <v-card>-->
+<!--                                        <v-card-title>Select Offer</v-card-title>-->
+<!--                                        <v-divider></v-divider>-->
+<!--                                        <v-card-text style="height: 300px" class="pl-0 pr-0 pt-3">-->
+<!--                                            <v-card-actions class="pl-12 pr-12">-->
+<!--                                                <v-icon class="mr-3" medium>{{icons.mdiCurrencyUsd}}</v-icon>-->
+<!--                                                <v-spacer></v-spacer>-->
+<!--                                                <v-icon class="ml-3" size="22px">mdi-watch</v-icon>-->
+<!--                                            </v-card-actions>-->
+<!--                                            <v-list rounded>-->
+<!--                                                <v-list-item-group v-model="dialogItem">-->
+<!--                                                    <v-list-item v-for="(n, k) in result[i].vendors[j].offers" :key="k">-->
+<!--                                                        <v-list-item-content>-->
+<!--                                                            <v-card-actions class="pl-8 pr-8">-->
+<!--                                                                <span>{{n.price}}</span>-->
+<!--                                                                <v-spacer></v-spacer>-->
+<!--                                                                <span>{{n.turnoverTime}}</span>-->
+<!--                                                            </v-card-actions>-->
+<!--                                                        </v-list-item-content>-->
+<!--                                                    </v-list-item>-->
+<!--                                                </v-list-item-group>-->
+<!--                                            </v-list>-->
+<!--                                        </v-card-text>-->
+<!--                                        <v-divider></v-divider>-->
+<!--                                        <v-card-actions>-->
+<!--                                            <v-btn color="blue darken-1" text-->
+<!--                                                   @click="dialog[i][j] = false">Close-->
+<!--                                            </v-btn>-->
+<!--                                            <v-btn color="blue darken-1" text-->
+<!--                                                   @click="dialogSave(i, j)">Save-->
+<!--                                            </v-btn>-->
+<!--                                        </v-card-actions>-->
+<!--                                    </v-card>-->
+<!--                                </v-dialog>-->
                             </v-card>
                         </v-hover>
                     </v-row>
                 </v-col>
-                <p>{{permanentElevation}} e</p>
+                <p>{{dialogItem}}</p>
             </v-row>
         </v-container>
     </div>
@@ -123,11 +146,16 @@
                 fileName: this.$store.state.StoreFile.name,
                 icons: {
                     mdiCurrencyUsd
-                }
+                },
+                dialogItem: null,
+                dialog: []
             }
         },
         computed: {
             result() {
+                return this.$store.state.StoreSearchResult.result;
+            },
+            resultBody() {
                 return this.$store.state.StoreSearchResult;
             },
             vendors() {
@@ -135,6 +163,12 @@
             },
             permanentElevation() {
                 return this.$store.state.StorePermanentElevation;
+            }
+        },
+        methods: {
+            dialogSave(i, j) {
+                this.dialog[i][j] = false;
+                this.$store.state.StoreSearchResult.result[i].vendors[j].offers[this.dialogItem].selected = true;
             }
         }
 

7b127041b9c2fc6654ab83c8ec47f89812868001
gledidoci <docigledi@gmail.com>
2020-01-27 23:38:36 +0100

    IDTClient is done.

--- Backend/Pinger/IDTClient.py
+++ Backend/Pinger/IDTClient.py
@@ -1,29 +1,31 @@
+import requests
+import json
 class IDTClient: 
     # Constructur for a IDTClient ()
     # Takes as input the configuration's parameters 
     
-    def __init__(self, identity_server, token_server, screening_server, idt_username, idt_password, grant_username, grant_password, client_id, shared_secret, token, scope, timeout = 60): 
-        self.identity_server = identity_server
+    def __init__(self, token_server, screening_server, idt_username, idt_password, client_id, client_secret, scope, token = "", timeout = 60):
         self.token_server = token_server
         self.screening_server = screening_server
         self.idt_username = idt_username
         self.idt_password = idt_password
-        self.grant_username = grant_password
         self.client_id = client_id
-        self.shared_secret = shared_secret
+        self.client_secret = client_secret
         self.token = token
-        if(self.token == "No_Token"):
-            self.getToken()
         self.scope = scope
         self.timeout = timeout
+        if(self.token == ""):
+            self.getToken()
            
     # Get Token
     def getToken(self):
-        data = {'grant_type': 'password', 'username': self.grant_username, 'password': self.grant_password, 'scope': self.scope}
-        r = requests.post(self.token_server, data, auth=requests.auth.HTTPBasicAuth(self.client_id, shared_secret), timeout = self.timeout)
-        access_token = json.loads(r.text)['access_token']
+        data = {'grant_type': 'password', 'username': self.idt_username, 'password': self.idt_password, 'scope': self.scope}
+        r = requests.post(self.token_server, data, auth=requests.auth.HTTPBasicAuth(self.client_id, self.client_secret), timeout = self.timeout)
+        if(not('access_token' in r.json())):
+            raise KeyError("Access token could not be generated. Check your credentials.")
+        access_token = r.json()['access_token']
         self.token = access_token
-    
+        
     # Use API-Endpoint
     def screening(self, listOfSequences):
         constructsList = []
@@ -33,9 +35,8 @@ class IDTClient:
                 "Sequence": construct["sequence"],
               }
             constructsList.append(sequence)
+        print(constructsList)
         resp = requests.post(self.screening_server,
-                  headers={'Authorization': 'Bearer {}'.format(self.token),
-                           'Content-Type': 'application/json; charset=utf-8'},
-                  constructsList, timeout = self.timeout)
+                  headers={'Authorization': 'Bearer {}'.format(self.token), 'Content-Type': 'application/json; charset=utf-8'}, json=constructsList, timeout = self.timeout)
         result = resp.json()
         return result

c284a941a0bb2257baec7f2091d8c6f797a603ee
gledidoci <docigledi@gmail.com>
2020-01-31 00:41:32 +0100

    IDT Pinger. More tests will follow

--- tests/runAllTests.sh
+++ tests/runAllTests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Initialize virtual Environment" 
+echo "Initialize virtual Environment"
 python3 -m venv venv
 
 echo "Install dependencies"
@@ -10,5 +10,7 @@ echo "Install dependencies"
 echo "run tests"
 venv/bin/python3 validatortest.py
 venv/bin/python3 compositepingertest.py
-venv/bin/python3 geneartclienttest.py
-venv/bin/python3 geneartpingertest.py
+#venv/bin/python3 geneartclienttest.py
+#venv/bin/python3 geneartpingertest.py
+venv/bin/python3 idtclienttest.py
+venv/bin/python3 idtpingertest.py

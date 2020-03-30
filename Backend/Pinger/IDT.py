bdd5262227dc4b2d7ec1f32708e868847388a903
gledidoci <docigledi@gmail.com>
2020-03-30 20:25:00 +0200

    IDT ORDERING DONE

--- Backend/Pinger/IDT.py
+++ Backend/Pinger/IDT.py
@@ -2,16 +2,21 @@ import requests
 from .Pinger import *
 from .Validator import *
 import json
+import uuid
+from datetime import datetime, timezone
+import xml.etree.ElementTree as ET
+
 class IDTClient: 
     # Constructor for a IDTClient ()
     # Takes as input the configuration's parameters 
-    def __init__(self, token_server, screening_server, idt_username, idt_password, client_id, client_secret, scope, token = "YOUR_TOKEN", timeout = 60):
+    def __init__(self, token_server, screening_server, idt_username, idt_password, client_id, client_secret, shared_secret, scope, token = "YOUR_TOKEN", timeout = 60):
         self.token_server = token_server
         self.screening_server = screening_server
         self.idt_username = idt_username
         self.idt_password = idt_password
         self.client_id = client_id
         self.client_secret = client_secret
+        self.shared_secret = shared_secret
         self.token = token
         self.scope = scope
         self.timeout = timeout
@@ -62,6 +67,127 @@ class IDTClient:
         result = resp.json()
         return result
 
+    # This method takes as input a listOfSequences and it is used to send a SOAP-Request to the API endpoint and complete an order.
+    def postorder(self, listOfSequences):
+
+        names = [x["name"] for x in listOfSequences]
+        sequences = [x["sequence"] for x in listOfSequences]
+
+        constructsList = list(zip(names, sequences))
+        header = u"""<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:idt="http://www.idtdna.com/">
+	<soapenv:Body>
+		<idt:PostPurchaseOrder>
+			<PurchaseOrder xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://schemas.modhul.com/cXML/1.2.008/OrderRequest" payloadID=""" + '"' + str(uuid.uuid4()) + '"' + u""" timestamp=""" + '"' + str(datetime.now(timezone.utc).isoformat()) + '"' + u""">
+				<Header>
+					<From>
+						<Credential domain="GBADnaScanner">
+							<Identity>DNAScannerClient</Identity>
+						</Credential>
+					</From>
+					<To>
+						<Credential domain="IDTDNA.com">
+							<Identity>IDT</Identity>
+						</Credential>
+					</To>
+					<Sender>
+						<Credential domain=""" + '"' + self.idt_username + '"' + u""">idt_username
+							<Identity>""" + self.client_id + u"""</Identity>
+							<SharedSecret>""" + self.shared_secret + u"""</SharedSecret>
+						</Credential>
+					</Sender>
+				</Header>
+				<Request>
+					<OrderRequest>
+						<OrderRequestHeader orderID="21603">
+							<ShipTo>
+								<Address isoCountryCode="US">
+									<Name>My Organization</Name>
+									<PostalAddress name="My Organization">
+										<DeliverTo>Mary Smith</DeliverTo>
+										<Street>1234 Elm St</Street>
+										<Street>220, Main Lab</Street>
+										<City>Middletown</City>
+										<State>Iowa</State>
+										<PostalCode>50222</PostalCode>
+										<Country isoCountryCode="US">US</Country>
+									</PostalAddress>
+									<Email name="">mary.smith@myorganization.com</Email>
+									<Phone>
+										<TelephoneNumber>
+											<CountryCode/>
+											<Number>(319) 904-8222</Number>
+										</TelephoneNumber>
+									</Phone>
+								</Address>
+							</ShipTo>
+							<BillTo>
+								<Address isoCountryCode="USA">
+									<Name>My Organization</Name>
+									<PostalAddress name="">
+										<Street>999 Oak St</Street>
+										<Street>Accounts Payable</Street>
+										<City>Middletown</City>
+										<State>IA</State>
+										<PostalCode>50222</PostalCode>
+										<Country isoCountryCode="USA">USA</Country>
+									</PostalAddress>
+									<Email name="">ap@myorganization.com</Email>
+									<Phone>
+										<TelephoneNumber>
+											<CountryCode/>
+											<Number>(319) 652-0322</Number>
+										</TelephoneNumber>
+									</Phone>
+								</Address>
+							</BillTo>
+						</OrderRequestHeader>"""
+        for construct in constructsList:
+            seqPart = "\n" + u"""						<ItemOut quantity="1">
+							<ItemDetail>
+								<Extrinsic>
+									<GBlockSpecification>
+										<Name>""" + construct[0] + u"""</Name>
+										<SequenceToManufacture>""" + construct[1] + u"""</SequenceToManufacture>
+										<TermsOfUseDisclosure>
+											<IsToxin>false</IsToxin>
+											<ToxinDescription>Describe toxin here. </ToxinDescription>
+											<IsOriginatedFromPlantOrAnimalPathogen>false</IsOriginatedFromPlantOrAnimalPathogen>
+											<PlantOrAnimalPathogenDescription>Describe pathogen here. </PlantOrAnimalPathogenDescription>
+											<IsInfectious>false</IsInfectious>
+											<InfectiousDescription>Describe infectious info here. </InfectiousDescription>
+											<IsEtiologicAgent>false</IsEtiologicAgent>
+											<EtiologicAgentDescription>Describe etiologic agent here. </EtiologicAgentDescription>
+											<InterferesWithBacterialHost>false</InterferesWithBacterialHost>
+											<BacterialHostInterferenceDescription>Describe bacterial interference here.</BacterialHostInterferenceDescription>
+											<Signature>Mary Smith</Signature>
+										</TermsOfUseDisclosure>
+										<Services/>
+									</GBlockSpecification>
+								</Extrinsic>
+							</ItemDetail>
+						</ItemOut>"""
+            header = header + seqPart
+        footer = u"""					</OrderRequest>
+				</Request>
+			</PurchaseOrder>
+		</idt:PostPurchaseOrder>
+	</soapenv:Body>
+</soapenv:Envelope>"""
+        request = header + "\n" + footer
+        encoded_request = request.encode('utf-8')
+
+        headers = {"Content-Type": "text/xml; charset=UTF-8",
+           "SOAPAction": "http://www.idtdna.com/PostPurchaseOrder"}
+
+        resp = requests.post(url="http://stage.idtdna.com/orderintegration/cxml/service.asmx",
+                     headers = headers,
+                     data = encoded_request,
+                     verify=False)
+
+
+        result = resp.text
+        return result
+
 #
 #   The IDT Pinger
 #
@@ -76,7 +202,7 @@ class IDT(BasePinger):
     # Constructur for an IDT-Pinger
     # Takes as input the log-in parameters.
     #
-    def __init__(self, idt_username, idt_password, client_id, client_secret, token_server = token_server_default, screening_server = screening_server_default, token = token_default, scope = scope_default, timeout = timeout_default):
+    def __init__(self, idt_username, idt_password, client_id, client_secret, shared_secret, token_server = token_server_default, screening_server = screening_server_default, token = token_default, scope = scope_default, timeout = timeout_default):
         self.running = False
         self.token_server = token_server
         self.screening_server = screening_server
@@ -84,13 +210,14 @@ class IDT(BasePinger):
         self.idt_password = idt_password
         self.client_id = client_id
         self.client_secret = client_secret
+        self.shared_secret = shared_secret
         self.scope = scope
         self.timeout = timeout
         self.token = token # Set the token (Token may or may not be valid)
         self.client = IDTClient(self.token_server, 
                       self.screening_server, self.idt_username, self.idt_password,
                       self.client_id, 
-                      self.client_secret, self.scope, 
+                      self.client_secret, self.shared_secret, self.scope, 
                       self.token, self.timeout)
         self.token = self.client.token # Set the token (Token is now valid because it was generated using the client)
         self.offers = []
@@ -157,7 +284,7 @@ class IDT(BasePinger):
                     for j in range(len(response[i])):
                         messageText = messageText + response[i][j]["Name"] + "."
                     message = Message(MessageType.SYNTHESIS_ERROR, messageText)
-                    self.validator.validate(message)           
+                self.validator.validate(message)           
                 seqOffer = SequenceOffers(seqInf[i], [Offer(messages = [message])])
                 self.validator.validate(seqOffer)
                 offers.append(seqOffer)
@@ -174,7 +301,97 @@ class IDT(BasePinger):
         except Exception as err:
             self.running = False
             raise UnavailableError from err
+
+    #
+    #   Desc:   Create a request to trigger an order.
+    #
+    #   @param offerIds
+    #           Type ArrayOf(int). Id of the Offer to Order. Ids must be unique.
+    #
+    #   @results
+    #           Type Entities.Order. Representation of the order.
+    #
+    #   @throws InvalidInputError
+    #           if input parameter are not like expected (see parameter definition above).
+    #
+    #   @throws IsRunningError
+    #           if the Pinger is already running. You have to wait until it is finished.
+    #
+    #   @throws UnavailableError
+    #           if authentication response not matches pattern or not received.
+    #           Maybe the base url of the API is wrong? API could be only temporary
+    #           unavailable.
+    #
+    def order(self, offerIds):
         
+        # Check pinger is not running
+        if(self.isRunning()):
+            raise IsRunningError("Pinger is currently running and can not perform a other action")
+        
+        # Check type of offersIds
+        if (not isinstance(offerIds, list)):
+            raise InvalidInputError("offerIds is not a list")
+        for id in offerIds:
+            if (not isinstance(id, int)):
+                raise InvalidInputError("offerIds contains not-integer value")
+
+        self.running = True
+
+        try:
+            # List to collect tuples of type (SequenceInformation)
+            offersToBuy = []
+
+            # find offers with id in given offerIds
+            for sequenceOffer in self.offers:
+                for offer in sequenceOffer.offers:
+                    # match
+                    if offer.key in offerIds:
+                        # add to list
+                        offersToBuy.append(sequenceOffer.sequenceInformation)
+
+            if len(offersToBuy) != len(offerIds):
+                raise InvalidInputError("Some of the offerIds are not found")
+            
+            print("Order", len(offersToBuy), "sequences at IDT")
+            order = Order(orderType = OrderType.MESSAGE)
+
+            constructsList = []
+            for construct in offersToBuy:
+                sequence = {
+                    "name": construct.name,
+                    "sequence": construct.sequence,
+                  }
+                constructsList.append(sequence)
+
+            orderResult = self.client.postorder(constructsList)
+            dom = ET.fromstring(orderResult)
+            returntext = dom.find('.//{http://www.idtdna.com/}ReturnText').text
+            returncode = dom.find('.//{http://www.idtdna.com/}ReturnCode').text
+            ordernumber = dom.find('.//{http://www.idtdna.com/}OrderNumber').text
+            
+            if(returncode != str(200)):
+                raise UnavailableError
+            message = "Return Text = {}. Return Code = {}. Order Number = {}.".format(returntext, returncode, ordernumber)
+
+            messageorder = MessageOrder(message = message)
+            self.running = False
+            return messageorder
+
+        except InvalidInputError as err:
+            self.running = False
+            raise InvalidInputError from err
+        except requests.exceptions.RequestException as err:
+            self.running = False
+            raise UnavailableError("Request got a error") from err
+        except UnavailableError as err:
+            self.running = False
+            raise UnavailableError from err
+        except AttributeError as err:
+            self.running = False
+            raise UnavailableError("Ordering went wrong.") from err
+        except Exception as err:
+            self.running = False
+            raise UnavailableError from err
     #
     #   Checks if the Pinger is Running.
     #
